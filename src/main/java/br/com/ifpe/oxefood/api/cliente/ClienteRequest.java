c0fa5a34c7153df267504d056622c3f7634636b9
ArmandoMartins1 <armando.amds97@gmail.com>
2023-12-05 17:45:09 -0300

    reformulação do repositorio

---
+++ src/main/java/br/com/ifpe/oxefood/api/cliente/ClienteRequest.java
@@ -0,0 +1,49 @@
+package br.com.ifpe.oxefood.api.cliente;
+
+import java.time.LocalDate;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import br.com.ifpe.oxefood.modelo.cliente.Cliente;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClienteRequest {
+
+   private String nome;
+
+   @NotBlank(message = "O Email é de preenchimento obrigatório")
+   @Email
+   private String email;
+
+   @JsonFormat(pattern = "dd/MM/yyyy")
+   private LocalDate dataNascimento;
+
+   private String cpf;
+
+   private String foneCelular;
+
+   private String foneFixo;
+
+   public Cliente build() {
+
+      return Cliente.builder()
+            .email(email)
+            .nome(nome)
+            .dataNascimento(dataNascimento)
+            .cpf(cpf)
+            .foneCelular(foneCelular)
+            .foneFixo(foneFixo)
+            .build();
+   }
+
+}
---
+++ src/main/java/br/com/ifpe/oxefood/modelo/produto/ProdutoService.java
@@ -0,0 +1,68 @@
+package br.com.ifpe.oxefood.modelo.produto;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProdutoService {
+    @Autowired
+    private ProdutoRepository repository;
+
+    @Transactional
+    public Produto save(Produto produto) {
+
+        produto.setHabilitado(Boolean.TRUE);
+        produto.setVersao(1L);
+        produto.setDataCriacao(LocalDate.now());
+        return repository.save(produto);
+    }
+
+    public List<Produto> findAll() {
+
+        return repository.findAll();
+    }
+
+    public Produto findById(Long id) {
+
+        return repository.findById(id).get();
+    }
+
+    /* Transactional para alteração do banco */
+    @Transactional
+    public void update(Long id, Produto produtoAlterado) {
+
+        Produto produto = repository.findById(id).get();
+        produto.setCodigo(produtoAlterado.getCodigo());
+        produto.setTitulo(produtoAlterado.getTitulo());
+        produto.setDescricao(produtoAlterado.getDescricao());
+        produto.setValorUnitario(produtoAlterado.getValorUnitario());
+        produto.setTempoEntregaMinimo(produtoAlterado.getTempoEntregaMinimo());
+        produto.setTempoEntregaMaximo(produtoAlterado.getTempoEntregaMaximo());
+        /* altera a verção começa de um e vai em diante */
+        produto.setVersao(produto.getVersao() + 1);
+        repository.save(produto);
+    }
+
+    @Transactional
+    public void delete(Long id) {
+
+        Produto produto = repository.findById(id).get();
+        produto.setHabilitado(Boolean.FALSE);
+        produto.setVersao(produto.getVersao() + 1);
+
+        repository.save(produto);
+    }
+
+    public List<Produto> listarTodos() {
+        return null;
+    }
+
+    public Produto obterPorID(Long id) {
+        return null;
+    }
+}
